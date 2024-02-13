47ac481159d19799ae80b1d956eeee010f94532b
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-02-13 21:07:48 +0100

    Testing command with args

--- lua/redis/fetch.lua
+++ lua/redis/fetch.lua
@@ -3,25 +3,32 @@ local M = {}
 
 -- Fetches todo tasks from the database and
 -- prints the output.
-function M.fetch_keys()
-  command = "redis-cli -h 172.17.0.3 keys '*'"
+function M.fetch_keys(params)
+  host = 'localhost'
+  if(params ~= '') and (params ~= nil) then
+    host = params
+  end
+
+  command = "redis-cli -h "..host.." keys '*'"
   args = {}
   
   local jobid = vim.fn.jobstart(command,
       {
           on_stdout = function(chanid, data, name)
+              output = ""
               for i, k in ipairs(data) do
-                print (k)
+                if (k ~= '') then
+                  output = output .. k .. ", "
+                end
               end
-
-              --print("keys: " .. vim.inspect(data))
+              if (output ~= '') then
+                output = output:sub(1, -3)
+                print("keys: "..output)
+              end
           end,
-          -- on_stderr = function(chanid, data, name)
-          --     print("stderr, data:" .. vim.inspect(data))
-          -- end,
-          -- on_exit = function(id, exitcode, event)
-          --     print("exit, exitcode:" .. vim.inspect(exitcode))
-          -- end,
+          on_stderr = function(chanid, data, name)
+            print("error: "..vim.inspect(data))
+          end,
       }
   )
   vim.fn.jobwait({ jobid })
--- lua/redis/init.lua
+++ lua/redis/init.lua
@@ -1,14 +1,30 @@
 -- Imports the plugin's additional Lua modules.
 local fetch = require("redis.fetch")
 
+local function setup(parameters)
+end
 -- Creates an object for the module. All of the module's
 -- functions are associated with this object, which is
 -- returned when the module is called with `require`.
-local M = {}
+--local M = {}
 
 -- Routes calls made to this module to functions in the
 -- plugin's other modules.
-M.fetch_keys = fetch.fetch_keys
+--M.fetch_keys = fetch.fetch_keys
+vim.api.nvim_create_user_command(
+  'ListKeys',
+  function(parameters)
+    print(vim.inspect(parameters))
+    print(parameters.fargs[1])
+    print(parameters.fargs[2])
+    fetch.fetch_keys(parameters.args)
+  end,
+  {bang = false, desc = 'list redis keys', nargs='*'}
+)
 
-return M
+return {
+  setup = setup
+}
+
+--return M
     
