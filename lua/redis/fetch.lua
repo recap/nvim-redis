05eef9ac34b6bc4c712d040ad696a2a7b62f9bd0
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-02-14 16:53:00 +0100

    added HGET command

--- lua/redis/fetch.lua
+++ lua/redis/fetch.lua
@@ -1,33 +1,53 @@
 -- Creates an object for the module.
 local M = {}
 
--- Fetches todo tasks from the database and
--- prints the output.
-function M.fetch_keys(params)
-  host = 'localhost'
-  if(params ~= '') and (params ~= nil) then
-    host = params
+local function is_empty(s)
+  return ((s == '') or (s == nil))
+end
+
+local function get_output_as_string(data)
+  output = ""
+  for i, k in ipairs(data) do
+    if (k ~= '') then
+      output = output .. k .. ", "
+    end
+  end
+  if (output ~= '') then
+    output = output:sub(1, -3)
+  end
+  return output
+end
+
+local function handle_cmd_error(chanid, data, name)
+  output = get_output_as_string(data)
+
+  if is_empty(output) then
+    return
   end
 
-  command = "redis-cli -h "..host.." keys '*'"
-  args = {}
+  print("Error executing cmd: "..command)
+  print(output)
+end
+
+
+function M.fetch_keys()
+
+  host = vim.g.redis_host
+  port = vim.g.redis_host_port
+
+  command = "redis-cli -h "..host.." -p "..port.." keys '*'"
   
   local jobid = vim.fn.jobstart(command,
       {
           on_stdout = function(chanid, data, name)
-              output = ""
-              for i, k in ipairs(data) do
-                if (k ~= '') then
-                  output = output .. k .. ", "
-                end
-              end
-              if (output ~= '') then
-                output = output:sub(1, -3)
-                print("keys: "..output)
-              end
+            output = get_output_as_string(data)
+            if is_empty(output) then
+              return
+            end
+            print("Keys: "..output)
           end,
           on_stderr = function(chanid, data, name)
-            print("error: "..vim.inspect(data))
+            handle_cmd_error(chanid, data, name)
           end,
       }
   )
@@ -35,5 +55,70 @@ function M.fetch_keys(params)
 
 end
 
+function M.get_key(key)
+  if is_empty(key) then
+    return
+  end
+
+  host = vim.g.redis_host
+  port = vim.g.redis_host_port
+
+  command = "redis-cli -h "..host.." -p "..port.." get "..key
+
+  no_output = false
+  
+  local jobid = vim.fn.jobstart(command,
+      {
+          on_stdout = function(chanid, data, name)
+            output = get_output_as_string(data)
+            if is_empty(output) then
+              return
+            end
+            print(key.." => "..output)
+            no_output = true
+          end,
+          on_stderr = function(chanid, data, name)
+            handle_cmd_error(chanid, data, name)
+          end,
+      }
+  )
+  vim.fn.jobwait({ jobid })
+  if no_output == false then
+    print("Key not found: "..key)
+  end
+end
+
+function M.hget_key(key, field)
+  if is_empty(key) or is_empty(field) then
+    return
+  end
+
+  host = vim.g.redis_host
+  port = vim.g.redis_host_port
+
+  command = "redis-cli -h "..host.." -p "..port.." hget "..key.." "..field
+
+  no_output = false
+  
+  local jobid = vim.fn.jobstart(command,
+      {
+          on_stdout = function(chanid, data, name)
+            output = get_output_as_string(data)
+            if is_empty(output) then
+              return
+            end
+            print(key..","..field.." => "..output)
+            no_output = true
+          end,
+          on_stderr = function(chanid, data, name)
+            handle_cmd_error(chanid, data, name)
+          end,
+      }
+  )
+  vim.fn.jobwait({ jobid })
+  if no_output == false then
+    print("Hkey not found: "..key..","..field)
+  end
+end
 return M
     
--- lua/redis/init.lua
+++ lua/redis/init.lua
@@ -2,18 +2,24 @@
 local fetch = require("redis.fetch")
 local argparse = require("redis.argparse")
 
+-- Set defaults. 
 local function setup(parameters)
+  vim.g.redis_host = 'localhost'
+  vim.g.redis_host_port = 6379
+  vim.g.redis_password = ""
+  vim.g.redis_user = ""
 end
--- Creates an object for the module. All of the module's
--- functions are associated with this object, which is
--- returned when the module is called with `require`.
---local M = {}
+
+local function is_empty(s)
+  return ((s == '') or (s == nil))
+end
 
 -- Routes calls made to this module to functions in the
 -- plugin's other modules.
 
+-- List all redis keys
 vim.api.nvim_create_user_command(
-  'RedisListKeys',
+  'RListKeys',
   function(parameters)
     args = argparse.parse(parameters.fargs)
     if argparse.exists(args, 'host') then
@@ -21,30 +27,63 @@ vim.api.nvim_create_user_command(
     elseif vim.g.redis_host ~= nil then
       fetch.fetch_keys(vim.g.redis_host)
     else
-      print("missing host argument!")
+      print("Missing host argument!")
     end
   end,
-  {bang = false, desc = 'list redis keys', nargs='*'}
+  {bang = false, desc = 'list redis keys', nargs=0}
 )
 
+-- Get a redis key value
 vim.api.nvim_create_user_command(
-  'RedisHost',
+  'RGetKey',
   function(parameters)
-    if (parameters.args ~= '') and (parameters.args ~= nil) then
-      vim.g.redis_host = parameters.args
-      print("set redis_host: "..vim.g.redis_host)
-    else
-      if (vim.g.redis_host ~= nil) then
-        print("redis_host: "..vim.g.redis_host)
-      end
+    if is_empty(parameters.args) then
+      return
     end
+    fetch.get_key(parameters.args)
   end,
-  {bang = false, desc = 'list redis keys', nargs='?'}
+  {bang = false, desc = 'get redis key value', nargs='?'}
+)
+
+-- Get a redis key value
+vim.api.nvim_create_user_command(
+  'RGetHKey',
+  function(parameters)
+    --print(vim.inspect(parameters))
+    if #parameters.fargs ~= 2 then
+      print("Wrong no of params!")
+      return
+    end
+    key = parameters.fargs[1]
+    field = parameters.fargs[2]
+    fetch.hget_key(key, field)
+  end,
+  {bang = false, desc = 'get redis key value', nargs='*'}
+)
+
+-- Set redis parameters e.g. host, port, password and user
+vim.api.nvim_create_user_command(
+  'RSetParams',
+  function(parameters)
+
+    args = argparse.parse(parameters.fargs)
+    if argparse.exists(args, 'host') then
+      vim.g.redis_host = args.host
+    end
+    if argparse.exists(args, 'port') then
+      vim.g.redis_host_port = args.port
+    end
+    if argparse.exists(args, 'pass') then
+      vim.g.redis_password = args.pass
+    end
+    if argparse.exists(args, 'user') then
+      vim.g.redis_user = args.user
+    end
+
+  end,
+  {bang = false, desc = 'list redis keys', nargs='*'}
 )
 
 return {
   setup = setup
 }
-
---return M
-    
